19a61040c812dfc2ae3dc8440add743507772d0a
Taha-Firoz <i170323@nu.edu.pk>
2020-04-20 00:01:15 +0500

    Added gitignores and exposed testcases

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,11 +1,14 @@
+#[test]
 fn bmp_color_asm(){
     assert_eq!("picture", "picture");
 }
 
+#[test]
 fn bmp_mono_asm(){
     assert_eq!("picture", "picture");
 }
 
+#[test]
 fn text_to_asm(){
     assert_eq!("picture", "picture");
 }
